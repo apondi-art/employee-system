35472f1ac247ef69affad337b0b5cfdb56663cfc
apondi-art <aiyanarich64@gmail.com>
2026-04-19 22:44:39 +0300

    feat:add mapper that is used to map dto to entity , enriry to dto

---
+++ src/main/java/com/employee/employee_system/MAPPER/EmployeeMapper.java
@@ -0,0 +1,54 @@
+package com.employee.employee_system.MAPPER;
+
+import com.employee.employee_system.DTO.EmployeeRequestDto;
+import com.employee.employee_system.DTO.EmployeeResponseDto;
+import com.employee.employee_system.entity.Employee;
+import org.springframework.stereotype.Component;
+
+
+@Component  // Spring-managed bean, can be @Autowired anywhere
+public class EmployeeMapper {
+
+    // Called by Service before returning data to Controller
+    public EmployeeResponseDto toResponseDto(Employee e) {
+        return EmployeeResponseDto.builder()
+                .id(e.getId())
+                .firstName(e.getFirstName())
+                .lastName(e.getLastName())
+                .email(e.getEmail())
+                .department(e.getDepartment())
+                .salary(e.getSalary())
+                .dateOfJoining(e.getDateOfJoining())
+                .active(e.getActive())
+                .createdAt(e.getCreatedAt())
+                .updatedAt(e.getUpdatedAt())
+                .build();
+    }
+
+    // Called by Service before saving to DB
+    public Employee toEntity(EmployeeRequestDto dto) {
+        return Employee.builder()
+                .firstName(dto.getFirstName())
+                .lastName(dto.getLastName())
+                .email(dto.getEmail())
+                .department(dto.getDepartment())
+                .salary(dto.getSalary())
+                .dateOfJoining(dto.getDateOfJoining())
+                .active(dto.getActive() != null ? dto.getActive() : true)
+                .build();
+        // id, createdAt, updatedAt are NOT set here — DB/JPA handles them
+    }
+
+    // Applies changes from RequestDto onto an EXISTING entity
+    // Used by PUT (full update)
+    public void updateEntityFromDto(EmployeeRequestDto dto, Employee e) {
+        e.setFirstName(dto.getFirstName());
+        e.setLastName(dto.getLastName());
+        e.setEmail(dto.getEmail());
+        e.setDepartment(dto.getDepartment());
+        e.setSalary(dto.getSalary());
+        e.setDateOfJoining(dto.getDateOfJoining());
+        e.setActive(dto.getActive());
+        // @PreUpdate will fire automatically and set updatedAt
+    }
+}
