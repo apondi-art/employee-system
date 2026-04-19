26a7b74c6580309ca3931e831c2fa8d53029f857
apondi-art <aiyanarich64@gmail.com>
2026-04-19 22:22:02 +0300

    feat:create dto folder, adding dto request and response

---
+++ src/main/java/com/employee/employee_system/DTO/EmployeeRequestDto.java
@@ -0,0 +1,43 @@
+package com.employee.employee_system.DTO;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+@Data
+public class EmployeeRequestDto {
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 50, message = "First name must be max 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 50, message = "Last name must be max 50 characters")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be a valid format e.g. user@domain.com")
+    private String email;
+
+    @NotBlank(message = "Department is required")
+    @Size(max = 100)
+    private String department;
+
+    @NotNull(message = "Salary is required")
+    @DecimalMin(value = "0.00", message = "Salary cannot be negative")
+    private BigDecimal salary;
+
+    @NotNull(message = "Date of joining is required")
+    @PastOrPresent(message = "Join date cannot be in the future")
+    private LocalDate dateOfJoining;
+
+    // If client omits this field, it defaults to true
+    private Boolean active = true;
+}
---
+++ src/main/java/com/employee/employee_system/DTO/EmployeeResponseDto.java
@@ -0,0 +1,30 @@
+package com.employee.employee_system.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmployeeResponseDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String department;
+    private BigDecimal salary;
+    private LocalDate dateOfJoining;
+    private Boolean active;
+
+    // These are read-only — client cannot set them
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
